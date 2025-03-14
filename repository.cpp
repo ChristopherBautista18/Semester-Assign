65bbfc3fba4954eda65f37fa07acdc7ebd7821bc
ChristopherBautista18 <2024161716@ub.edu.bz>
2025-03-14 16:09:07 -0600

    Add files via upload

---
+++ repository.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+int number;
+
+cout <<" enter a number: "<< endl;
+cin >> number;
+
+if( number % 2 == 0) {
+    cout <<"The number "<< number <<" is even" << endl;
+} else {
+    cout <<"The number "<< number <<" is odd" << endl;
+
+}
+
+ return 0;
+}
+
+
+
+
+
